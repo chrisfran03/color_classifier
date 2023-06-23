4ebbc927e30545d95b0dc0371a3cda0f9ef5df60
chrisfran03 <270155795@yoobeestudent.ac.nz>
2023-06-24 00:50:10 +1200

    added web functionality

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 from return_color import predict_color
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"   
+    return "hello world!"
 
-# @app.route('/return_color')
+@app.route('/return_color', methods=['POST'])
+def return_color():
+    data = request.get_json()
+    red = data.get('red')
+    green = data.get('green')
+    blue = data.get('blue')
+    
+    if red is None or green is None or blue is None:
+        return 'Missing RGB values', 400
+    
+    color = predict_color(red, green, blue)
+    return color
+# @app.route('/return_color' , methods=['POST'])
 # def return_color(red,green,blue):
 #   color = predict_color(red,green,blue)
 #   return color
